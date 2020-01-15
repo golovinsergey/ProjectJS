8b4932a5407b8b22b2ecb0120e1fee64a9c3f507
Sergey <sg0970777639@gmail.com>
2020-01-15 15:47:50 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,51 @@
+'use strict';
+
+/*var LeftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+
+console.log(4/0);
+
+let persone = {
+    name: "Diana",
+    age: 40,
+    isMarried: true
+};
+console.log(persone.isMarried);
+
+let arr = ['Hi','Hello','By'];
+
+console.log(arr[0]);*/
+
+//alert("Hi Yaroslav");
+
+/*let answer = confirm("Are you here?")
+
+console.log(answer);*/
+
+//let answer = prompt("Have you 18 ?", "Yes")
+
+//console.log(answer);
+
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
+
+console.log(15%2);
+console.log("2" == 2);//Сравнивает по значению и выдает true
+console.log("2" === 2);//Сравнивает по значению и по типу данн-х и выдает false
+
+let inChecked = true,
+    inClosed = false;
+
+console.log(inChecked && inClosed);
+
+console.log(inChecked || inClosed);
+
+var myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
