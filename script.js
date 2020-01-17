240575a047591eda05da7bfdd7a1741c64743085
Sergey <sgo970777639@gmail.com>
2020-01-17 16:41:34 +0200

    mod scriptjs

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ console.log(answer);*/
 
 //console.log(answer);
 
-let incr = 10,
+/*let incr = 10,
     decr = 10;
 
 incr++;
@@ -49,3 +49,26 @@ console.log(inChecked || inClosed);
 
 var myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello world!';
+*/
+
+let money = prompt("Ваш бюджет на месяц?", ''),
+	time = prompt('Введите дату в формате YYYY-MM-DD', '');
+
+let appData = {
+	budget: money,
+	expenses: {},
+	optionalExpenses: {},
+	income: [],
+	timeData: time,
+	savings: false
+};
+
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a2 = prompt("Во сколько обойдется?", ''),
+	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a4 = prompt("Во сколько обойдется?", '');
+
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
+
+alert(appData.budget / 30);
